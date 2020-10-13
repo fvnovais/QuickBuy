3538c9c5afab54d97c14b9ae5d2d070ced1300c2
Fernando Vieira Novais <fernando.novais@gmail.com>
2020-10-13 12:00:38 -0300

    Regras de negócios das validação das entidades

--- QuickBuy.Dominio/Entidades/Pedido.cs
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -1,10 +1,11 @@
 ﻿using QuickBuy.Dominio.ObjetoDeValor;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace QuickBuy.Dominio.Entidades
 {
-    public class Pedido
+    public class Pedido : Entidade
     {
         public int Id { get; set; }
         public DateTime DataPedido { get; set; }
@@ -21,5 +22,20 @@ namespace QuickBuy.Dominio.Entidades
         public ICollection<ItemPedido> ItensPedido { get; set; }
         public int FormaPagamentoId { get; set; }
         public FormaPagamento FormaPagamento { get; set; }
+
+        public override void Validate()
+        {
+            LimparMensagensValidacao();
+
+            if(!ItensPedido.Any()) 
+            {
+                AdicionarCritica("Crítica - Pedido não pode ficar sem item de pedido");
+            }
+
+            if(string.IsNullOrEmpty(CEP))
+            {
+                AdicionarCritica("Crítica - CEP deve estar preenchido");
+            }
+        }
     }
 }
